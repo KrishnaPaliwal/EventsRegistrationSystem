1829fa79fe690e8e640f2648c9d4447dd1177c0a
Krishna <krishna.paliwal85@gmail.com>
2023-08-08 23:31:12 +0530

    Made compatible with Java-11

--- src/main/java/com/event/registration/Application.java
+++ src/main/java/com/event/registration/Application.java
@@ -1,13 +1,16 @@
 package com.event.registration;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource({"classpath:application.properties"})
+@EnableAutoConfiguration(exclude = { HibernateJpaAutoConfiguration.class })
 public class Application extends SpringBootServletInitializer {   
     
     @Override
